08f9a0b07018ad078e1a82a956abedef967ec881
Phil Hudson <philhudson91@gmail.com>
2019-12-12 22:24:16 +0800

    fix broken router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Redirect} from "react-router-dom";
 import NavTop from './components/pages/navbar.component';
 import SideNav from './components/pages/sidenav.component';
 import Main from './components/pages/main.component';
 import Login from './components/pages/login.component';
+import Home from './components/pages/home.component';
+import Album from './components/pages/album.component';
+import SearchResults from './components/pages/searchResults.component';
+import Player from './components/pages/player.component';
+import Playlists from './components/pages/playlists.component';
+import Playlist from './components/pages/playlist.component';
+import Artist from './components/pages/artist.component';
 
 function App() {
+    const [playerToggled, setPlayerToggled] = useState(false);
+
     return (<Router>
             <div className="App" style={{
                 display: 'flex',                   /* defines flexbox */
@@ -18,7 +27,31 @@ function App() {
                 <SideNav/>
                 <Switch>
                     <Route exact path='/' component={Login}/>
-                    <Main/>
+                    {/*TODO sort this mess out*/}
+                    <Route>
+                        <div style={{
+                            display: 'flex',                   /* defines flexbox */
+                            flexDirection: 'column',          /* top to bottom */
+                            flex: 1,
+                        }}>
+                            {playerToggled === false && (
+                                <div style={{'marginLeft': '160px'}}>
+                                    <Switch>
+                                        <Route exact path='/' component={Login}/>
+                                        <Route exact path='/home' component={Home}/>
+                                        <Route exact path='/album/:id' component={Album}/>
+                                        <Route exact path='/search:query' component={SearchResults}/>
+                                        <Route exact path='/player' component={Player}/>
+                                        <Route exact path='/playlists' component={Playlists}/>
+                                        <Route exact path='/playlist/:id' component={Playlist}/>
+                                        <Route exact path='/artist/:id' component={Artist}/>
+
+                                    </Switch>
+                                </div>
+                            )}
+                            <Player setPlayerToggled={setPlayerToggled}/>
+                        </div>
+                    </Route>
                 </Switch>
 
             </div>
