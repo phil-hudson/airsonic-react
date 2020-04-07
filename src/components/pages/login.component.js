0bafcac7339e7e9149941203584af1aed869c64c
Phil Hudson <>
2020-04-07 18:02:33 +0800

    fix broken login update dep

--- src/components/pages/login.component.js
+++ src/components/pages/login.component.js
@@ -13,7 +13,7 @@ const Login = (props) => {
 
     const username = localStorage.getItem('username');
     const server = localStorage.getItem('server');
-    const [inputs, setInputs] = useState({"username": username, "password": "", "serverAddress": server, 'remember': false});
+    const [inputs, setInputs] = useState({"username": username, "password": "", "serverAddress": server});
     const history = useHistory()
     const handleInputChange = (event) => {
         event.persist();
@@ -36,7 +36,7 @@ const Login = (props) => {
         };
     };
 
-    const loginRequest = (server, username, token, salt, remember) => {
+    const loginRequest = (server, username, token, salt) => {
         const appName = 'ReactFrontend';
         fetch(`${server}/rest/ping.view?u=${username}&t=${token}&s=${salt}&v=1.15.0&c=${appName}&f=json`, {
             crossDomain: true,
@@ -48,13 +48,12 @@ const Login = (props) => {
                 console.log('success');
                 console.log(data);
                 if (data['subsonic-response']['status'] === 'ok') {
+                    console.log('successful auth');
                     // store token
-                    if (remember === true) {
-                        localStorage.setItem('server', server);
-                        localStorage.setItem('username', username);
-                        localStorage.setItem('token', token);
-                        localStorage.setItem('salt', salt);
-                    }
+                    localStorage.setItem('server', server);
+                    localStorage.setItem('username', username);
+                    localStorage.setItem('token', token);
+                    localStorage.setItem('salt', salt);
                     props.setSuccessfulAuth(true);
                     history.push("/home")
                 } else {
@@ -67,39 +66,41 @@ const Login = (props) => {
     return (
         <div className="auth-wrapper">
             <div className="auth-inner">
-        <form onSubmit={handleSubmit}>
-            <h3>Sign In</h3>
+                <form onSubmit={handleSubmit}>
+                    <h3>Sign In</h3>
 
-            <div className="form-group">
-                <label>Username</label>
-                <input type="text" className="form-control" placeholder="Enter username" name="username"
-                       onChange={handleInputChange} value={inputs.username} required/>
-            </div>
+                    <div className="form-group">
+                        <label>Username</label>
+                        <input type="text" className="form-control" placeholder="Enter username" name="username"
+                               onChange={handleInputChange} value={inputs.username} required/>
+                    </div>
 
-            <div className="form-group">
-                <label>Password</label>
-                <input type="password" className="form-control" placeholder="Enter password" name="password"
-                       onChange={handleInputChange} value={inputs.password} required/>
-            </div>
+                    <div className="form-group">
+                        <label>Password</label>
+                        <input type="password" className="form-control" placeholder="Enter password" name="password"
+                               onChange={handleInputChange} value={inputs.password} required/>
+                    </div>
 
-            <div className="form-group">
-                <label>Server address</label>
-                <input type="text" className="form-control" placeholder="Enter server address" name="serverAddress"
-                       onChange={handleInputChange} value={inputs.serverAddress} required/>
-            </div>
+                    <div className="form-group">
+                        <label>Server address</label>
+                        <input type="text" className="form-control" placeholder="Enter server address"
+                               name="serverAddress"
+                               onChange={handleInputChange} value={inputs.serverAddress} required/>
+                    </div>
 
-            <div className="form-group">
-                <div className="custom-control custom-checkbox">
-                    <input type="checkbox" className="custom-control-input" name="remember" id="remember"/>
-                    <label className="custom-control-label" htmlFor="remember">Remember me</label>
-                </div>
-            </div>
+                    {/*<div className="form-group">*/}
+                    {/*    <div className="custom-control custom-checkbox">*/}
+                    {/*        <input type="checkbox" className="custom-control-input" name="remember" id="remember"*/}
+                    {/*               defaultChecked={inputs.remember}/>*/}
+                    {/*        <label className="custom-control-label" htmlFor="remember">Remember me</label>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
 
-            <button type="submit" className="btn btn-primary btn-block">Submit</button>
-            <p className="forgot-password text-right">
-                Forgot <a href="#">password?</a>
-            </p>
-        </form>
+                    <button type="submit" className="btn btn-primary btn-block">Submit</button>
+                    <p className="forgot-password text-right">
+                        Forgot <a href="#">password?</a>
+                    </p>
+                </form>
             </div>
         </div>
     );
